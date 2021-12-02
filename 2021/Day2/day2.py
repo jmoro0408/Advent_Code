f78732a45a32b847d6cd4c9293557c7467de4fae
jmoro0408 <james.moro.11@gmail.com>
2021-12-02 12:37:33 +0000

    Day 2 Part 2

--- 2021/Day2/day2.py
+++ 2021/Day2/day2.py
@@ -16,5 +16,23 @@ for line in dive:
     elif direction == "up":
         position[1] -= magnitude
 
-print(f"final position: {position}")
-print(f"final result: {position[0] * position[1]}")
+print(f"Part 1 final position: {position}")
+print(f"Part 1 final result: {position[0] * position[1]}")
+
+
+# part2
+position = [0, 0, 0]  # horizontal, depth, aim
+for line in dive:
+    space_index = line.index(" ")
+    direction = line[:space_index]
+    magnitude = int(line[space_index:])
+    if direction == "forward":
+        position[0] += magnitude
+        position[1] += position[2] * magnitude
+    elif direction == "down":
+        position[2] += magnitude
+    elif direction == "up":
+        position[2] -= magnitude
+
+print(f"Part 2 final position: {position}")
+print(f"Part 2 final result: {position[0] * position[1]}")
