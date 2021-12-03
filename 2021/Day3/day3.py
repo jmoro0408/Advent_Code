e291819bb7d83f10c40040c39db4cc9fb372bc73
jmoro0408 <james.moro.11@gmail.com>
2021-12-03 18:08:42 +0000

    Day3 Part2

--- 2021/Day3/day3.py
+++ 2021/Day3/day3.py
@@ -1,26 +1,40 @@
-from collections import Counter
+import numpy as np
+import pandas as pd
 
+data = np.genfromtxt("Day3/input3.csv", dtype=str)
+data_df = pd.DataFrame(data=[list(i) for i in data])
 
-def frequent(lst):
-    data = Counter(lst)
-    return data.most_common(1)[0][0]
+bin_num = data_df.mode().sum().sum()
+gamma = int(bin_num, 2)
+inverted_bin = "".join("1" if x == "0" else "0" for x in bin_num)
+epsilon = int(inverted_bin, 2)
 
+power_consumption = gamma * epsilon
 
-with open("day3/input3.txt", "r") as f:
-    data = f.readlines()
-    data = [(line.rstrip()) for line in data]
-    count = {k: [] for k in range(len(data[0]))}
-    most_common_bin = {k: 0 for k in range(len(data[0]))}
-    for j, num in enumerate(data):
-        for index, value in enumerate(num):
-            count[index].append(value)
-            most_common_bin[index] = frequent(count[index])
+print(f"Power consumption: {power_consumption}")
 
-    gamma_bin = "".join([str(value) for value in most_common_bin.values()])
-    epsilon_bin = "".join("1" if x == "0" else "0" for x in gamma_bin)
-    gamma = int(gamma_bin, 2)
-    epsilon = int(epsilon_bin, 2)
+# Part2
+oxy_df = data_df.copy()
+co2_df = data_df.copy()
 
-    power_consumption = gamma * epsilon
+for value in oxy_df.columns:
+    most_common = oxy_df[value].mode()
+    if len(most_common) > 1:
+        oxy_df = oxy_df[oxy_df[value].values == "1"]
+    else:
+        oxy_df = oxy_df[oxy_df[value].values == most_common.iloc[0]]
+    if len(oxy_df) == 1:
+        ox = int(oxy_df.iloc[0].sum(), 2)
+        break
 
-    print(f"Power consumption: {power_consumption}")
+for value in co2_df.columns:
+    mode = co2_df[value].mode()
+    if len(mode) > 1:
+        co2_df = co2_df[co2_df[value].values == "0"]
+    else:
+        co2_df = co2_df[co2_df[value].values == str(1 - int(mode.iloc[0]))]
+    if len(co2_df) == 1:
+        CO2 = int(co2_df.iloc[0].sum(), 2)
+        break
+
+print(f"Lift support rating: {ox * CO2}")
