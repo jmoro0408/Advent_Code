0adffbae0442972312c05f5087062e853c64f66e
James Moro <james.moro@aecom.com>
2022-12-20 17:20:51 +0000

    day6

--- 2022/day6.py
+++ 2022/day6.py
@@ -4,4 +4,17 @@ INPUT_DIR = r"inputs/day6.txt"
 
 arr = read_input(INPUT_DIR)
 
-print(arr)
+
+a=list(arr[:4])
+
+for i in range(4,len(arr)):
+    if len(set(a))==4:print(i);break
+    a.pop(0)
+    a.append(arr[i])
+
+## Part two
+a=list(arr[:14])
+for i in range(14,len(arr)):
+    if len(set(a))==14:print(i);break
+    a.pop(0)
+    a.append(arr[i])
